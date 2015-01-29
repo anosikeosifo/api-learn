fc45282d767dc86ede76e151b1c2f37b3dfd3362
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-01-29 07:30:37 +0100

    Add shoulda-matchers for spec refactor

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,9 @@ describe User do
 	it	{ should respond_to(:password) }
 	it	{ should respond_to(:password_confirmation) }
 
-	it	{ should be_valid }
+	it { should be_valid }
+	it { should validate_presence_of(:email) }
+	it { should validate_uniqueness_of(:email) }
+	it { should validate_confirmation_of(:password) }
+	it { should allow_value('example@domain.com').for(:email) }
 end
-
-# RSpec.describe User, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
