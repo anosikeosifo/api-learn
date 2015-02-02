6eefd427fd256d05d8d6c3d332bf45ab74183a13
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-02-02 09:48:05 +0100

    Add unique token generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  before_create(:generate_auth_token!) #this is a callback
+
+  validates :auth_token, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def generate_auth_token!
+  	begin
+  		self.auth_token = Devise.friendly_token
+  	end while self.class.exists?(auth_token: auth_token)
+  	#this do-while loop structure ensures that tha token is generated if the user doesnt have any or 
+  	# if the one previously generated was invalid(already in use)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,33 @@ describe User do
 	it	{ should respond_to(:password) }
 	it	{ should respond_to(:password_confirmation) }
 
+	#it should also respond to auth_token
+	it { should respond_to(:auth_token) }
+
 	it { should be_valid }
 	it { should validate_presence_of(:email) }
 	it { should validate_uniqueness_of(:email) }
 	it { should validate_confirmation_of(:password) }
 	it { should allow_value('example@domain.com').for(:email) }
+
+	#auth_token should also be unique for users
+	it { should validate_uniqueness_of(:auth_token) } 
+
+
+
+	describe "#genetate_aunthentication_token" do
+		it "generates a unique token" do
+			Devise.stub(:friendly_token).and_return("osifo_unuque_token")
+			@user.generate_auth_token!
+			expect(@user.auth_token).to eql "osifo_unuque_token"
+		end
+
+		it "generates another token when one has already been taken" do
+			existing_user = FactoryGirl.create(:user, auth_token: "token_in_use")
+
+			@user.generate_auth_token!
+			expect(@user.auth_token).not_to eql existing_user.auth_token #coz its already in use
+		end
+
+	end
 end
